bb043707bdccc56e21a6c2aa15a9e00df95c927e
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-26 16:07:33 +0100

    zad 4

--- basics_pgg/zad_4.py
+++ basics_pgg/zad_4.py
@@ -11,6 +11,16 @@ Podaj dystans Warszawa-Gdańsk: 420
 Podaj cene paliwa: 4.55
 Spalanie na 100 km: 5.5
 Koszt przejazdu Warszawa-Gdańsk to 105.11 PLN
+
+100 km  - spalanie
+dystans - x
 """
 miasto_a = input('Podaj miasto A: ')
 miasto_b = input('Podaj miasto B: ')
+dystans = float(input(f'Podaj dystans {miasto_a}-{miasto_b}'))
+cena = float(input('Podaj cene paliwa: '))
+spalanie = float(input('Spalanie na 100 km: '))
+
+koszt = dystans * spalanie / 100.0 * cena
+
+print(f'Koszt przejazdu {miasto_a}-{miasto_b} to {koszt:.2f} PLN')
