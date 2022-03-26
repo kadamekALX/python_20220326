142a5de9c37e1154bf039514404785fde01a35a5
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-26 13:05:24 +0100

    zad 2

--- basics_pgg/zad_1.py
+++ basics_pgg/zad_1.py
@@ -5,3 +5,10 @@ napisz program obliczający pole trapezu
 o długości podstaw 3 i 9 oraz wysokości 6.5.
 """
 
+podstawa_a = 3
+podstawa_b = 9
+wysokosc = 6.5
+
+# kolejnosc wykonywania operatorow: https://docs.python.org/3/reference/expressions.html#operator-precedence
+pole_trapezu = 1 / 2 * (podstawa_a + podstawa_b) * wysokosc
+print(pole_trapezu)
