7d07deb360b4d008d6d8b8ea4ca50e19e2ddacb6
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-26 14:44:09 +0100

    zad 3

---
+++ basics_pgg/zad_3.py
@@ -0,0 +1,13 @@
+"""
+Napisz program wyliczający kwotę należną za zakupiony
+towar na podstawie ceny za kilogram oraz liczby zakupionych
+kilogramów.
+Do przechowywania informacji o cenie oraz liczbie kilogramów
+użyj zmiennych.
+Wypisz wszystkie informacje na konsolę.
+
+Przykładowy komunikat programu:
+Podaj cene za kg: 10.0
+Podaj wage: 2.5
+Należność: 25.0
+"""
