d6882f1abceb1ed76dfb34c866f3a8ee6aa65fdd
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-26 14:04:04 +0100

    zad 2

---
+++ basics_pgg/zad_2.py
@@ -0,0 +1,9 @@
+"""
+Napisz program wypisujący na konsolę Twoje imię i wzrost.
+Do przechowywania informacji o Twoim imieniu i wzroście użyj
+zmiennych.
+
+Przykładowy komunikat programu:
+Imię: Jan
+Wzrost: 180
+"""
