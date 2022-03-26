b8c0b2fadedeb5973f22804066eaad388febdc6b
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-26 15:25:08 +0100

    zad 3

--- basics_pgg/zad_3.py
+++ basics_pgg/zad_3.py
@@ -11,3 +11,22 @@ Podaj cene za kg: 10.0
 Podaj wage: 2.5
 Należność: 25.0
 """
+
+cena = float(input('Podaj cene za kg: '))
+waga = float(input('Podaj wage: '))
+
+naleznosc = round(cena * waga, 2)
+print(type(naleznosc))
+
+
+print('Naleznosc:', naleznosc, 'PLN')  # 3 argumenty przekazane do printa
+
+print('Naleznosc: ' + str(naleznosc) + ' PLN')
+
+naleznosc_napis = 'Naleznosc: ' + str(naleznosc) + ' PLN'
+print(naleznosc_napis)  # 1 argument przekazany do printa
+
+# f-string, formatted string, https://docs.python.org/3/library/string.html#format-specification-mini-language
+naleznosc = cena * waga
+naleznosc_napis = f'Naleznosc: {naleznosc:_^15.2f} PLN'
+print(naleznosc_napis)
