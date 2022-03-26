9a9a27df681893dc7eda6197fa7b5e1177ebbff0
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-26 15:52:55 +0100

    zad 4

--- basics_pgg/zad_4.py
+++ basics_pgg/zad_4.py
@@ -5,10 +5,12 @@ przez użytkownika liczby kilometrów, ceny paliwa oraz spalania.
 Zapytaj użytkownika także o nazwy miejscowości.
 
 Przykładowe komunikaty programu:
-Miasto A: Warszawa
-Miasto B: Gdańsk
-Dystans Warszawa-Gdańsk: 420
-Cena paliwa: 4.55
+Podaj miasto A: Warszawa
+Podaj miasto B: Gdańsk
+Podaj dystans Warszawa-Gdańsk: 420
+Podaj cene paliwa: 4.55
 Spalanie na 100 km: 5.5
-Koszt przejazdu Warszawa-Gdańsk to 105 PLN
+Koszt przejazdu Warszawa-Gdańsk to 105.11 PLN
 """
+miasto_a = input('Podaj miasto A: ')
+miasto_b = input('Podaj miasto B: ')
