654970fdd73574a170b6d4d1d5b4c16f9bdd3903
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-27 11:46:24 +0200

    zad 10

---
+++ basics_pgg/zad_10_2.py
@@ -0,0 +1,33 @@
+"""
+Napisz program, który na podstawie dwóch podanych liczb obliczy wynik zadanej operacji
+(dodawanie, odejmowanie, mnożenie, dzielenie). W przypadku podania nieprawidłowej operacji
+program ma wyświetlić odpowiedni komunikat o błędzie.
+
+Przykładowy komunikat programu:
+Podaj pierwszą liczbę: 10
+Podaj drugą liczbę: 5
+Podaj rodzaj operacji (+, -, *, /): +
+Wynik: 10+5=15
+"""
+
+liczba_1 = float(input('Podaj pierwszą liczbę: '))
+liczba_2 = float(input('Podaj drugą liczbę: '))
+operacja = input('Podaj rodzaj operacji (+, -, *, /): ')
+
+wynik = None
+
+if operacja == '+':
+    wynik = liczba_1 + liczba_2
+elif operacja == '-':
+    wynik = liczba_1 - liczba_2
+elif operacja == '*':
+    wynik = liczba_1 * liczba_2
+elif operacja == '/':
+    if liczba_2 != 0:
+        wynik = liczba_1 / liczba_2
+
+if wynik == None:
+    print('Nieprawidlowa operacja.')
+else:
+    print(f'Wynik: {liczba_1}{operacja}{liczba_2}={wynik}')
+
