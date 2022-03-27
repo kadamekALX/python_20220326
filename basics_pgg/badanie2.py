32c52aac7341be1adc629e8cc364e612561adbbb
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-27 10:26:34 +0200

    walidacja danych uzytkownika

--- basics_pgg/badanie2.py
+++ basics_pgg/badanie2.py
@@ -1,8 +1,11 @@
 badanie = int(input("W skali od 1 do 10 jak bardzo poleciłbys szkolenie w ALX: "))
 
-if badanie >= 9:
-    print("promotorzy")
-elif badanie >=7:
-    print("neutralni")
+if badanie >= 1 and badanie <= 10:
+    if badanie >= 9:
+        print("promotorzy")
+    elif badanie >= 7:
+        print("neutralni")
+    else:
+        print("detraktorzy")
 else:
-    print("detraktorzy")
+    print('Podaj wartosc z zakresu 1-10')
