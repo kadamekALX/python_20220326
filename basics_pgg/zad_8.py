51b8fe217e0ff5ede645e24600618b069ba400cc
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-27 10:27:47 +0200

    zad 8

---
+++ basics_pgg/zad_8.py
@@ -0,0 +1,8 @@
+"""
+Napisz program, który zapyta uzytkownika o liczbę całkowitą.
+Jeżeli uzytkownik poda liczbę 10, wtedy wyświetl komunikat:
+Podałeś szczęśliwy numerek! Wygrałeś!
+W przeciwnym wypadku wyświetl:
+Spróbuj jeszcze raz!
+"""
+
