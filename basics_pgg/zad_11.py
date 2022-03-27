fb3c8aca2ce0dc5d89efe6f2657a36e18fa23339
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-27 12:12:32 +0200

    zad 12

--- basics_pgg/zad_11.py
+++ basics_pgg/zad_11.py
@@ -3,9 +3,11 @@ Napisz program obliczający kwadrat 100 pierwszych liczb całkowitych
 i wypisujący wyniki na konsolę.
 
 Przykładowe uruchomienie programu:
+Kwadrat liczby 1 to 1
 ...
 Kwadrat liczby 10 to 100
 ...
+Kwadrat liczby 100 to 10000
 """
 i = 1
 while i <= 100:
---
+++ basics_pgg/zad_12.py
@@ -0,0 +1,4 @@
+"""
+Napisz program obliczający średnią wartość temperatury w danym tygodniu
+na podstawie temperatur wprowadzonych przez użytkownika.
+"""
