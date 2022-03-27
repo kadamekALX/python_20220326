4caac07bdf980ce39ec19f4811ab5aa2690811b3
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-27 09:34:21 +0200

    zad 7

--- basics_pgg/zad_7.py
+++ basics_pgg/zad_7.py
@@ -2,3 +2,13 @@
 Napisz program, który na podstawie wprowadzonych wymiarów opakowania (x, y, z) obliczy
 jego objętość oraz sprawdzi, czy jest ona większa od 1 litra (1000 cm3).
 """
+
+x = int(input('Podaj x: '))
+y = int(input('Podaj y: '))
+z = int(input('Podaj z: '))
+
+objetosc = x * y * z
+czy_wieksze_od_litra = objetosc > 1000
+
+print(f'Objetosc: {objetosc}')
+print(f'Czy objetosc jest wieksza od 1l: {czy_wieksze_od_litra}')
