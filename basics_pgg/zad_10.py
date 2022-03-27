1af1649f001a4d70e8f2ff1be9d3d536eb1297fe
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-27 11:09:22 +0200

    zad 10

---
+++ basics_pgg/zad_10.py
@@ -0,0 +1,11 @@
+"""
+Napisz program, który na podstawie dwóch podanych liczb obliczy wynik zadanej operacji
+(dodawanie, odejmowanie, mnożenie, dzielenie). W przypadku podania nieprawidłowej operacji
+program ma wyświetlić odpowiedni komunikat o błędzie.
+
+Przykładowy komunikat programu:
+Podaj pierwszą liczbę: 10
+Podaj drugą liczbę: 5
+Podaj rodzaj operacji (+, -, *, /): +
+Wynik: 15
+"""
