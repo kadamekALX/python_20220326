39d3fe082b008776386189a182a2c64f2e4194c6
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-27 15:53:02 +0200

    zad 2

---
+++ collections_pgg/zad_2.py
@@ -0,0 +1,8 @@
+"""
+Napisz program obliczający średnią wartość z podanych przez użytkownika liczb.
+Do przechowywania liczb użyj listy. Pozwól na wprowadzenie 10 liczb.
+Skorzystaj z funkcji wbudowanej sum().
+"""
+
+liczby = []  # [] - tworzy pusta liste
+
