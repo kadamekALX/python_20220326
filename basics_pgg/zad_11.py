001a38ee70191714e3724edc33f5997e56c3872e
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-27 12:08:30 +0200

    zad 11

--- basics_pgg/zad_11.py
+++ basics_pgg/zad_11.py
@@ -7,3 +7,8 @@ Przykładowe uruchomienie programu:
 Kwadrat liczby 10 to 100
 ...
 """
+i = 1
+while i <= 100:
+    print(f'Kwadrat liczby {i} to {i ** 2}')
+    # i = i + 1
+    i += 1 # robi dokladnie to samo co i = i + 1, mamy tez -=, *=, /=
