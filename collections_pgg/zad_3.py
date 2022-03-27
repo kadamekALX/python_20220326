8ec6e90961f84d2a23992b7244270d90ff98822d
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-03-27 16:32:42 +0200

    zad 3, for

--- collections_pgg/zad_3.py
+++ collections_pgg/zad_3.py
@@ -7,3 +7,14 @@ liście liczb całkowitych.
 
 liczby = [1, 2, 3, -100, -10, 0, 4]
 
+dodatnie = 0
+ujemne = 0
+
+for liczba in liczby:
+    if liczba > 0:
+        dodatnie += 1
+    elif liczba < 0:
+        ujemne += 1
+
+print(f'Dodatnich: {dodatnie}')
+print(f'Ujemnych: {ujemne}')
