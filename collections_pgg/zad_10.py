60db60a68f2e6385a17fb6993915cf07c79b8f92
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-04-09 14:21:31 +0200

    zad 10

--- collections_pgg/zad_10.py
+++ collections_pgg/zad_10.py
@@ -6,9 +6,8 @@ w podanym przez użytkownika napisie.
 1. Robimy pusty słownik, w którym będziemy zliczać litery
 2. Wczytujemy napis od użytkownika
 3. Przechodzimy przez wszystkie znaki (for)
-  3a. jesli nie ma danego znaku w slowniku to dodajemy
+  3a. jesli nie ma danego znaku w slowniku to dodajemy go z wartoscia 1
   4b. jezeli jest to zwiększamy liczbe o 1
-
 
 slownik
 klucz  => wartość
@@ -17,9 +16,27 @@ litera => liczba wystąpień (int)
 Trzeba sprawdzić czy dana litera wystepuje w slowniku!
 
 Podaj napis: Ala
+wynik:
 {
     'a': 2,
     'l': 1
 }
 """
 
+napis = input('Podaj napis: ')
+wystapienia = {}
+
+try:
+    if len(napis) > 1:
+        pass  # pass - "null operation" - operacja, ktora nic nie robi
+except ValueError:
+    pass  # jak nie chce nic robic, kiedy powstanie wyjatek (Error)
+
+for litera in napis.lower():
+    if litera not in wystapienia:
+        wystapienia[litera] = 1
+    else:
+        wystapienia[litera] += 1
+
+for litera, liczba_wystapien in wystapienia.items():
+    print(f'{litera} - {liczba_wystapien}')
