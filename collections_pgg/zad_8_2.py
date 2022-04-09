e96a67945fbfc4bb851d84642a73b12722af441d
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-04-09 12:01:53 +0200

    zad 8

---
+++ collections_pgg/zad_8_2.py
@@ -0,0 +1,30 @@
+"""
+Napisz program zliczający liczbę znaków w podanym przez użytkownika napisie
+pomiędzy nawiasami <>. Nawiasy mogą wystąpić tylko raz.
+
+Przykład:
+Ala ma <kota>, a kot ma Alę
+4
+
+1. Sprawdzam liczbę < i > - powinno ich być po jednym,
+      jeżeli liczba tych nawiasów jest inna, to kończę program
+2. W pętli sprawdzam czy:
+      - mam zliczac
+      - mam przestać zliczać
+      - czy zliczanie jest włączone i wtedy zliczam
+"""
+
+napis = input('Podaj napis: ')
+
+if napis.count('<') != 1 or napis.count('>') != 1:
+    print('Nieprawidlowa liczba nawiasow <>')
+    exit()  # exit powoduje zamkniecie programu i to chcemy zrobic, bo dane sa nieprawidlowe
+
+# if napis.find('<') > napis.find('>'):
+if not (napis.find('<') < napis.find('>')):
+    print('Zla kolejnosc nawiasow, najpier < a pozniej >')
+    exit()
+
+liczba_znakow = napis.find('>') - napis.find('<') - 1
+
+print(f'W napisie "{napis}" znaleziono {liczba_znakow} znakow')
