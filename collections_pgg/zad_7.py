6a9bdfac5b1dcab7b66bbd6ab759794e2d66fe82
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-04-09 11:06:01 +0200

    zad 7

--- collections_pgg/zad_7.py
+++ collections_pgg/zad_7.py
@@ -10,3 +10,15 @@ Jak poradzić sobie ze zliczaniem dużych liter?
 1. Możemy je dodać do tupli z samogłoskami
 2. Użyć metody .lower()
 """
+
+napis = input('Podaj napis: ')
+
+samogloski = ['a', 'e', 'i', 'o', 'u', 'y']
+liczba_samoglosek = 0
+
+for litera in napis:
+    if litera in samogloski:
+        liczba_samoglosek += 1
+
+print(f'W napisie "{napis}" znajduje się {liczba_samoglosek} samoglosek')
+
