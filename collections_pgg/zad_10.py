fcb59b5cda4e166c3f13ba3a380e5bc56c492fa9
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-04-09 14:02:19 +0200

    zad 10

---
+++ collections_pgg/zad_10.py
@@ -0,0 +1,25 @@
+"""
+Napisz program zliczający liczbę wystąpień każdego znaku
+w podanym przez użytkownika napisie.
+
+
+1. Robimy pusty słownik, w którym będziemy zliczać litery
+2. Wczytujemy napis od użytkownika
+3. Przechodzimy przez wszystkie znaki (for)
+  3a. jesli nie ma danego znaku w slowniku to dodajemy
+  4b. jezeli jest to zwiększamy liczbe o 1
+
+
+slownik
+klucz  => wartość
+litera => liczba wystąpień (int)
+
+Trzeba sprawdzić czy dana litera wystepuje w slowniku!
+
+Podaj napis: Ala
+{
+    'a': 2,
+    'l': 1
+}
+"""
+
