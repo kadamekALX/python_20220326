68e024f081ae8a5951a3ece4b413834448e375d1
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-04-10 12:24:44 +0200

    zad 2

---
+++ functions_pgg/zad_2.py
@@ -0,0 +1,15 @@
+"""
+Napisz funkcję sprawdzającą, czy dana liczba jest liczbą pierwszą.
+Przykład użycia:
+> czy_jest_pierwsza(10)
+False
+> czy_jest_pierwsza(17)
+True
+
+Liczba pierwsza
+- bedzie intem
+- wieksza od 1
+- dzieli sie tylko przez 1 i przez sama siebie
+"""
+
+def czy_jest_pierwsza(liczba: int) -> bool:
