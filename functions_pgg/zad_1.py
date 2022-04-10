0033b07f62bdb51785bd7a17febfded9c426f65e
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-04-10 11:41:00 +0200

    zad 1

---
+++ functions_pgg/zad_1.py
@@ -0,0 +1,7 @@
+"""
+Stwórz kalkulator oparty o funkcje:
+1. Pobierz od użytkownika 2 liczby
+2. Zapytaj o działanie: (+, -, *, /)
+3. Na podstawie działania wykonujemy obliczenia i je pokazujemy
+"""
+
