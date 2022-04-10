94f03aa7bd137ad960a0a8f14324971ce360fc23
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-04-10 16:35:04 +0200

    zad 3

--- functions_pgg/zad_3.py
+++ functions_pgg/zad_3.py
@@ -16,6 +16,21 @@ pusty napis
 # TDD - Test Driven Development
 
 def wiecej_niz(napis: str, ile_znakow: int) -> set:
+    # napis = ala ma kota a kot ma ale
+    # ile_znakow = 3
+    wystapienia = {}
+    for litera in napis.lower():
+        if litera not in wystapienia:
+            wystapienia[litera] = 1
+        else:
+            wystapienia[litera] += 1
+
+    wynik = set()
+    for litera, liczba_wystapien in wystapienia.items():
+        if liczba_wystapien > ile_znakow:
+            wynik.add(litera)
+
+    return wynik
 
 
 def test_pusty_napis():
