9f6133992507feae358aaca4f172db28f217d380
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2022-04-10 11:41:28 +0200

    zad 1

--- functions_pgg/zad_1.py
+++ functions_pgg/zad_1.py
@@ -5,3 +5,19 @@ Stwórz kalkulator oparty o funkcje:
 3. Na podstawie działania wykonujemy obliczenia i je pokazujemy
 """
 
+liczba_1 = float(input('Podaj pierwszą liczbę: '))
+liczba_2 = float(input('Podaj drugą liczbę: '))
+operacja = input('Podaj rodzaj operacji (+, -, *, /): ')
+
+if operacja == '+':
+    wynik = liczba_1 + liczba_2
+elif operacja == '-':
+    wynik = liczba_1 - liczba_2
+elif operacja == '*':
+    wynik = liczba_1 * liczba_2
+elif operacja == '/':
+    wynik = liczba_1 / liczba_2
+else:
+    wynik = 'operacja nieprawidlowa'
+
+print(f'Wynik: {liczba_1}{operacja}{liczba_2}={wynik}')
