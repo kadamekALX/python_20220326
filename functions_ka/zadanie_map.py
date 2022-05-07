b25f4c5ff71b6db94280fa76817fc1356df90705
Krzysztof Adamek <kadamek@alx.pl>
2022-05-07 15:25:17 +0200

    zadanie_map

--- functions_ka/zadanie_map.py
+++ functions_ka/zadanie_map.py
@@ -2,3 +2,14 @@
 # przyjęty napis na listę intów
 # Haczyk: limit wierzy == 1
 # "1,2,3,4,5" -> [1,2,3,4,5]
+
+# napis = input("Podaj liczby rozdzielone przecinkami:")
+# lista = napis.split(',')
+# inty = []
+# for s in lista:
+#     inty.append(int(s))
+# print(inty)
+
+print(list(map(int, input("Podaj liczby rozdzielone przecinkami:").split(','))))
+
+# print(list(filter(lambda znak: znak.isdigit(), input("Podaj ciag znakow:"))))
