16e9b26136a3b3676e8efacc8da975d7dedb50fb
Krzysztof Adamek <kadamek@alx.pl>
2022-05-07 12:56:49 +0200

    zad6

--- functions_ka/zad_6.py
+++ functions_ka/zad_6.py
@@ -3,10 +3,45 @@
 # >>> splaszcz([1, 2, 3, [4, 5, [6]], 7])
 # [1, 2, 3, 4, 5, 6, 7]
 
-# isinstace(zmienna, typ) - sprawdza, czy `zmienna` jest typu `typ`
+# isinstance(zmienna, typ) - sprawdza, czy `zmienna` jest typu `typ`
+# if isinstance([1,2,3], list):
+#     print("[1,2,3] jest typu `list`")
+
+# lista = [1, 4.5, "asdf", 4, 5, 3.14, "qwer", "zxcv"]
+# wynik = {}
+# for x in lista:
+#     if type(x) in wynik:
+#         wynik[type(x)].append(x)
+#     else:
+#         wynik[type(x)] = [x]
+
+def s(lista):
+    while True:
+        wynik = []
+        czy_lista_w_liscie = False
+        for x in lista:
+            if isinstance(x, list):
+                czy_lista_w_liscie = True
+                wynik.extend(x)
+            else:
+                wynik.append(x)
+        if czy_lista_w_liscie:
+            lista = wynik
+        else:
+            return wynik
 
 def splaszcz(lista):
-    pass
+    wynik = []
+    for element in lista:
+        if isinstance(element, (list,tuple)):
+            # wynik.extend(splaszcz(element))
+            splaszczona = splaszcz(element)
+            for x in splaszczona:
+                wynik.append(x)
+        else:
+            wynik.append(element)
+    return wynik
+
 
 def test_simple():
     assert splaszcz([1,2,3,4]) == [1,2,3,4]
@@ -16,3 +51,8 @@ def test_pojedyncze_zagniezdzenie():
 
 def test_zadanie():
     assert splaszcz([1, 2, 3, [4, 5, [6]], 7]) == [1,2,3,4,5,6,7]
+
+
+if __name__ == "__main__":
+    print(s([1, 2, [3, 4], 5]))
+    print(s([1, 2, [[[[[[[[[[[[[[[[[[3, [4, 777]]]]]]]]]]]]]]]]]]], 5]))
