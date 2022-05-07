25561243f057fbe71414959e4c393b8fd0368bed
Krzysztof Adamek <kadamek@alx.pl>
2022-05-07 15:30:21 +0200

    map

--- functions_ka/map_filter.py
+++ functions_ka/map_filter.py
@@ -14,3 +14,14 @@ plus5 = lambda x: x + 5
 print(list(filter(lambda x: x % 2 == 0, lista)))
 
 # (lambda x, y: print(f"{x + y = }"))(5, 10) # od razu wywołuję funkcję anonimową
+
+
+def plus4(x):
+    print(f"Dodaję 4 do {x}")
+    return x + 4
+
+lista = [1,2,3,4]
+# for x in [plus4(i) for i in lista]:
+for x in map(plus4, lista):
+    print("Printuję:")
+    print(x)
