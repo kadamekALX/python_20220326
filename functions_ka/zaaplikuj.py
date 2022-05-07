08d1a91f34d44c663ea31a93c9f6b66de0f45b32
Krzysztof Adamek <kadamek@alx.pl>
2022-05-07 14:11:09 +0200

    odkomentowano test

--- functions_ka/zaaplikuj.py
+++ functions_ka/zaaplikuj.py
@@ -5,8 +5,8 @@
 def zaaplikuj(fun, lista):
     pass
 
-# def kwadrat(x):
-#     return x ** 2
-#
-# def test_zaaplikuj():
-#     assert zaaplikuj(kwadrat, [1, 2, 3, 4]) == [1, 4, 9, 16]
+def kwadrat(x):
+    return x ** 2
+
+def test_zaaplikuj():
+    assert zaaplikuj(kwadrat, [1, 2, 3, 4]) == [1, 4, 9, 16]
