e7c164b82bce589c660b3fe13163074afaa1b3b5
Krzysztof Adamek <kadamek@alx.pl>
2022-05-08 11:11:20 +0200

    Postac

--- oop_ka/zad_postac.py
+++ oop_ka/zad_postac.py
@@ -1,23 +1,36 @@
 class Postac:
     def __init__(self, imie, zdrowie):
-        pass
+        self.imie = imie
+        self.zdrowie = zdrowie
+        self.max_zdrowie = zdrowie
 
     def wypisz(self):
-        pass
+        print(f"{self.imie}, {self.zdrowie}/{self.max_zdrowie} HP")
 
     # zmniejsza zdrowie o dmg; zdrowie nie powinno spaść poniżej 0
     def otrzymaj_obrazenia(self, dmg):
-        pass
+        if dmg < 0:
+            self.wylecz(-dmg)
+            return
+        self.zdrowie -= dmg
+        if self.zdrowie < 0:
+            self.zdrowie = 0
 
     # przywraca `hp` utraconych punktów zdrowia;
     # zdrowie nie powinno przekroczyć maksymalnej wartosci
     # leczenie nie działa jesli postac nie zyje
     def wylecz(self, hp):
-        pass
+        if hp < 0:
+            self.otrzymaj_obrazenia(-hp)
+            return
+        if self.czy_zyje():
+            self.zdrowie += hp
+            if self.zdrowie > self.max_zdrowie:
+                self.zdrowie = self.max_zdrowie
 
     # zwraca iformację, czy postać żyje
     def czy_zyje(self):
-        pass
+        return self.zdrowie > 0
 
 rufus = Postac("Rufus", 120)
 rufus.wypisz() # Rufus, 120/120 HP
@@ -28,6 +41,8 @@ rufus.wypisz() # Rufus, 108/120 HP
 rufus.wylecz(30)
 rufus.wypisz() # Rufus, 120/120 HP
 rufus.otrzymaj_obrazenia(150)
+rufus.wypisz() # Rufus, 0/120 HP / Rufus, nie żyje
+rufus.wylecz(30)
 rufus.wypisz() # Rufus, 0/120 HP / Rufus, nie żyje
 
 p = Postac("Worek treningowy", 100)
