927a84ed3ee15b9f410894f695aeaace40d91637
Krzysztof Adamek <kadamek@alx.pl>
2022-05-08 10:18:52 +0200

    zad_2

--- oop_ka/zad_2.py
+++ oop_ka/zad_2.py
@@ -15,21 +15,33 @@
 
 class Employee:
     def __init__(self, imie, nazwisko, pensja):
-        pass
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.pensja = pensja
+        self.przepracowane_godziny = 0
 
     def register_time(self, h):
-        pass
+        if h <= 8:
+            self.przepracowane_godziny += h
+        else:
+            self.przepracowane_godziny += 8 + (h - 8) * 2
 
     def pay_salary(self):
-        pass
+        wynagrodzenie = self.przepracowane_godziny * self.pensja
+        self.przepracowane_godziny = 0
+        return wynagrodzenie
 
 def test_0():
     e = Employee("Jan", "Kowalski", 100)
+    assert e.pay_salary() == 0
     e.register_time(5)
     assert e.pay_salary() == 500
     assert e.pay_salary() == 0
     e.register_time(5)
     e.register_time(5)
     assert e.pay_salary() == 1000
+    e.register_time(5) # 0 nadgodzin
+    e.register_time(9) # 1 nadgodzina
+    assert e.pay_salary() == 1500
     e.register_time(10)
     assert e.pay_salary() == 1200
