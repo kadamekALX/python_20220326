0ec0f05e36d584d89b65f319ffd7f3947337c1d9
Krzysztof Adamek <kadamek@alx.pl>
2022-05-08 09:20:41 +0200

    zad_1

--- oop_ka/zad_1.py
+++ oop_ka/zad_1.py
@@ -1,14 +1,14 @@
 class Product:
-    def __init__(self, id, name, price):
+    def __init__(self, id, name, value):
         self.id = id
         self.name = name
-        self.price = price
+        self.price = value
 
     def print_info(self):
         print(f"Produkt \"{self.name}\", id: {self.id}, cena: {self.price} PLN")
 
 
-woda = Product(id=1, name="Woda", price=10.99)
+woda = Product(id=1, name="Woda", value=10.99)
 chleb = Product(2, "Chleb", 3.50)
 woda.print_info()
 chleb.print_info()
