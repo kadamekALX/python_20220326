8e58f5a5a61d83ed64bdd4d599df7adbf25d0387
Krzysztof Adamek <kadamek@alx.pl>
2022-05-21 11:58:04 +0200

    zad 7 - tresc

---
+++ oop_ka/zad_7.py
@@ -0,0 +1,13 @@
+from zad_2 import Employee
+
+# Zaimplementuj klasę PremiumEmployee, która będzie klasą
+# pochodną Employee. Klasa ta powinna umożliwiać dodatkowo
+# przyznawanie bonusów pracownikowi.
+# >>> employee = PremiumEmployee('Jan', 'Nowak', 100.0)
+# >>> employee.register_time(5)
+# >>> employee.give_bonus(1000.0)
+# >>> employee.pay_salary()
+# 1500.0
+
+class PremiumEmployee(Employee):
+    pass
