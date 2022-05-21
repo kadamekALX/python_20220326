848c54ecc5456994c42d04178ab05f0455dbeace
Krzysztof Adamek <kadamek@alx.pl>
2022-05-21 11:04:06 +0200

    mieszkania v2

--- oop_ka/mieszkania.py
+++ oop_ka/mieszkania.py
@@ -5,36 +5,58 @@
 
 class Osoba:
     def __init__(self, imie):
-        pass
+        self.imie = imie
+        self.adres = None
 
     def wypisz(self): # Jan, mieszka w Domek jednorodzinny
-        pass
+        if self.adres is None:
+            print(f"{self.imie}, bezdomny")
+        else:
+            print(f"{self.imie}, mieszka w {self.adres}")
 
     def wymelduj(self):
-        pass
+        if self.adres is not None:
+            self.adres.wymelduj(self)
+        self.adres = None
 
     def zamelduj(self, dom):
-        pass
+        self.wymelduj()
+        if dom is not None:
+            dom.zamelduj(self)
+        self.adres = dom
 
 class Dom:
     def __init__(self, nazwa):
-        pass
+        self.nazwa = nazwa
+        self.lokatorzy = []
+
+    def __str__(self):
+        return self.nazwa
 
     def wypisz_mieszkancow(self):
-        pass
+        print(f"{self.nazwa}:")
+        for m in self.lokatorzy:
+            print(f"\t{m.imie}")
 
     def zamelduj(self, osoba):
-        pass
+        self.lokatorzy.append(osoba)
 
     def wymelduj(self, osoba):
-        pass
+        self.lokatorzy.remove(osoba)
 
 
 jan = Osoba("Jan")
 adam = Osoba("Adam")
+adam.wymelduj()
 jan.wypisz() # Jan, bezdomny
 d = Dom("Domek jednorodzinny")
 jan.zamelduj(d)
 jan.wypisz() # Jan, zamieszkały w: Domek jednorodzinny
 adam.zamelduj(d)
 d.wypisz_mieszkancow() # Jan, Adam
+d2 = Dom("Chałupa")
+jan.zamelduj(d2)
+d2.wypisz_mieszkancow()
+d.wypisz_mieszkancow()
+jan.zamelduj(None)
+jan.wypisz()
