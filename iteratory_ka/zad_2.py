f452261b2fd3951c6f08127e8ccef6690ac38cea
Krzysztof Adamek <kadamek@alx.pl>
2022-05-21 14:52:01 +0200

    iteratory - zad 2

---
+++ iteratory_ka/zad_2.py
@@ -0,0 +1,11 @@
+# Zaimplementuj iterator zwracający jedynie samogłoski z zadanego
+# ciągu znaków:
+# Przykładowe użycie:
+# for char in Samogloski("Ala ma kota"):
+#     print(char)
+# Wynik:
+#     A
+#     a
+#     a
+#     o
+#     a
