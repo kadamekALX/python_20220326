b17f393fd592d39ef509cc1e509d9d47c3d5b109
Krzysztof Adamek <kadamek@alx.pl>
2022-05-21 15:57:11 +0200

    generatory - zad 3

--- iteratory_ka/zad_3.py
+++ iteratory_ka/zad_3.py
@@ -2,3 +2,13 @@
 # ciągu znaków:
 # Przykładowe użycie:
 # for char in vowels('ala ma kota a kot ma ale'):
+
+def samogloski(napis):
+    for litera in napis:
+        if litera.lower() in "aeiouy":
+            yield litera
+
+for x in samogloski("Ala ma kota"):
+    print("samogloska:", x)
+
+print(list(samogloski("Ala ma pieska")))
