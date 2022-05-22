f06e256c3960bd3f7b1c9d6e2f62df24e6607c5a
Krzysztof Adamek <kadamek@alx.pl>
2022-05-22 10:11:08 +0200

    pliki - zad 1

--- files_ka/zad_1.py
+++ files_ka/zad_1.py
@@ -5,3 +5,17 @@
 # $ python test.txt
 # 1: pierwsza linia pliku
 # 2: druga linia pliku
+
+# with open("plik.txt") as plik:
+#     tresc = plik.read()
+# for i, wiersz in enumerate(tresc.split('\n'), 1):
+#     print(f"{i}: {wiersz}")
+
+nazwa_pliku = input("Podaj nazwe pliku:")
+
+try:
+    with open(nazwa_pliku) as plik:
+        for i, w in enumerate(plik, 1):
+            print(f"{i}: {w.rstrip()}")
+except FileNotFoundError:
+    print("Nie ma takiego pliku!")
