fa735c6eb7987ed97e28493b7d11d3ed28f673d5
Krzysztof Adamek <kadamek@alx.pl>
2022-05-22 12:44:46 +0200

    stdlib - zad_1

--- stdlib_ka/zad_1.py
+++ stdlib_ka/zad_1.py
@@ -20,3 +20,19 @@ try:
         lista = json.load(plik)
 except FileNotFoundError:
     lista = []
+
+op = input("Jaką operację chcesz wykonać? [d-dodaj, w-wypisz]:")
+if op == 'd':
+    pracownik = {}
+    pracownik["imie"] = input("Podaj imie:")
+    pracownik["nazwisko"] = input("Podaj nazwisko:")
+    pracownik["rok_ur"] = int(input("Podaj rok urodzenia:"))
+    pracownik["pensja"] = int(input("Podaj pensję:"))
+    lista.append(pracownik)
+    with open("pracownicy.json", 'w') as plik:
+        json.dump(lista, plik, indent=4)
+elif op == 'w':
+    for nr, p in enumerate(lista, 1):
+        print(f"[{nr}] - {p['imie']} {p['nazwisko']}")
+else:
+    print("Nieprawidłowy wybór:", op)
