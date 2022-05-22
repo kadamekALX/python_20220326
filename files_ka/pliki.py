c9c96928388bfe3bf894b3c1d79b5bee5c2b7f4b
Krzysztof Adamek <kadamek@alx.pl>
2022-05-22 10:21:34 +0200

    pliki - zapisywanie

--- files_ka/pliki.py
+++ files_ka/pliki.py
@@ -11,3 +11,9 @@ with open("plik.txt") as otwarty_plik:
     tresc = otwarty_plik.read()
 print(type(tresc))
 print(repr(tresc))
+
+
+with open("wyjscie.txt", "w") as plik: # otwieramy plik do zapisu - jeśli pliku nie ma to zostanie stworzony. Jeśli jest to jego zawartosc jest usuwana
+    plik.write("Ala ma kota") # write nie stawia automatycznie '\n'
+    plik.write("\n")
+    plik.write("Ola ma psa")
