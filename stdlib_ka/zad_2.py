25697c1accee85187e1038687ad0b785e9b8de2d
Krzysztof Adamek <kadamek@alx.pl>
2022-05-22 14:35:05 +0200

    stdlib - zad 2 tresc - komentarze

--- stdlib_ka/zad_2.py
+++ stdlib_ka/zad_2.py
@@ -1,5 +1,5 @@
-Pobierz losowy dowcip o Chucku Norrisie ze strony https://api.chucknorris.io/  z wybranej prez użytkownika kategorii
-Przed pobraniem kategorii wyświetl listę wszystkich kategorii.
+# Pobierz losowy dowcip o Chucku Norrisie ze strony https://api.chucknorris.io/  z wybranej prez użytkownika kategorii
+# Przed pobraniem kategorii wyświetl listę wszystkich kategorii.
 
 from urllib.request import urlopen, Request
 import json
