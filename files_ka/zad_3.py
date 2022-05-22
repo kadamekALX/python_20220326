5c20a33718c0304a048388d6f802c143b5609d73
Krzysztof Adamek <kadamek@alx.pl>
2022-05-22 10:30:03 +0200

    zad 3 - tresc

---
+++ files_ka/zad_3.py
@@ -0,0 +1,7 @@
+# Napisz program wczytujący listę adresów email z pliku i tworzący
+# nowy plik z odfiltrowaną zawartością.
+# Wejściowy plik może zawierać duplikaty adresów, ten sam adres
+# pisany różną wielkością liter, linie zawierające białe znaki oraz
+# błędne adresy email (brak znaku @ lub występuje on wiele razy).
+# Wynikowy plik powinien zawierać unikalne, posortowane, poprawne
+# adresy email.
