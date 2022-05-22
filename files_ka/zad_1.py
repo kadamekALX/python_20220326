2a9f0e76bf431febaf04303d986f43d97ee366b8
Krzysztof Adamek <kadamek@alx.pl>
2022-05-22 09:35:41 +0200

    pliki - zad 1 tresc

---
+++ files_ka/zad_1.py
@@ -0,0 +1,7 @@
+# Napisz program wypisujący na konsolę zawartość wskazanego pliku
+# wraz z numerami linii. Obsłuż sytuację, gdy użytkownik nie poda
+# nazwy pliku lub poda błędną nazwę.
+# Przykład użycia:
+# $ python test.txt
+# 1: pierwsza linia pliku
+# 2: druga linia pliku
