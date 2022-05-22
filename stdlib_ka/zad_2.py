79419b1fb79b4d3a26d7d5595b85e3ba5df52091
Krzysztof Adamek <kadamek@alx.pl>
2022-05-22 15:20:08 +0200

    stdlib - zad 2

--- stdlib_ka/zad_2.py
+++ stdlib_ka/zad_2.py
@@ -3,3 +3,17 @@
 
 from urllib.request import urlopen, Request
 import json
+
+h = {"User-Agent": "Turboprzegladarka3000"}
+zapytanie = Request("https://api.chucknorris.io/jokes/categories", headers=h)
+with urlopen(zapytanie) as plik:
+    kategorie = json.load(plik)
+print(f"Dostępne kategorie: {' '.join(kategorie)}")
+wybor = input("Wprowadz wybor:")
+if wybor not in kategorie:
+    exit(1)
+
+zapytanie = Request(f"https://api.chucknorris.io/jokes/random?category={wybor}", headers=h)
+with urlopen(zapytanie) as plik:
+    zart = json.load(plik)
+print(zart['value'])
