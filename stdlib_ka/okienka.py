00e0b20206b83e0ba16c030a6c78cf65955ad6f4
Krzysztof Adamek <kadamek@alx.pl>
2022-05-22 16:00:07 +0200

    stdlib - aplikacja okienkowa

---
+++ stdlib_ka/okienka.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+def fun():
+    print("Przycisk został nacisniety.")
+    tresc = pole_tekstowe.get()
+    print(f"Tresc pola: {tresc}")
+    tekst.configure(text=tresc)
+
+root = tk.Tk()
+
+przycisk = tk.Button(master=root, text="Click me!", command=fun)
+przycisk.grid(row=0, column=0)
+
+pole_tekstowe = tk.Entry(master=root)
+pole_tekstowe.grid(row=1, column=1)
+
+tekst = tk.Label(master=root, text="To jest label")
+tekst.grid(row=0, column=1)
+
+root.mainloop()
