002c895cb9596c3b44341027641e9d9ad9a67317
Krzysztof Adamek <kadamek@alx.pl>
2022-05-22 11:10:16 +0200

    zad 3

--- files_ka/zad_3.py
+++ files_ka/zad_3.py
@@ -5,3 +5,20 @@
 # błędne adresy email (brak znaku @ lub występuje on wiele razy).
 # Wynikowy plik powinien zawierać unikalne, posortowane, poprawne
 # adresy email.
+
+# with open("emails.txt") as plik_in:
+#     with open("clean_emails.txt", 'w') as plik_out:
+#         plik_out.write("\n".join(sorted({w.strip().lower() for w in plik_in.read().split('\n') if w.count('@') == 1})))
+
+with open("emails.txt") as plik_in:
+    tresc = plik_in.read().split('\n')
+
+zbior = set()
+for wiersz in tresc:
+    if wiersz.count('@') == 1:
+        zbior.add(wiersz.strip().lower())
+
+with open("clean_emails.txt", 'w') as plik_out:
+    for email in sorted(zbior):
+        plik_out.write(email)
+        plik_out.write('\n')
