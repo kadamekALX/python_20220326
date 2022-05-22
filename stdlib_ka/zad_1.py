54b595d7106107595fb9c3d7b357289945d5a697
Krzysztof Adamek <kadamek@alx.pl>
2022-05-22 11:54:09 +0200

    stdlib - zad_1 treść

---
+++ stdlib_ka/zad_1.py
@@ -0,0 +1,22 @@
+# Napisz program obsługujący bazę danych pracowników. W bazie
+# danych przechowuj imię, nazwisko, email, rok urodzenia, pensję.
+# Skorzystaj z modułu json.
+# Przykład użycia:
+# $ python employees.py
+# Co chcesz zrobić? [d - dodaj, w - wypisz] d
+# Imie: Jan
+# Nazwisko: Nowak
+# Rok urodzenia: 1980
+# Pensja: 5000.0
+# $ python employees.py
+# Co chcesz zrobić? [d - dodaj, w - wypisz] w
+# Pracownicy:
+# - [1] Jan Nowak - rok: 1980, pensja: 5000.00 PLN
+
+import json
+
+try:
+    with open("pracownicy.json") as plik:
+        lista = json.load(plik)
+except FileNotFoundError:
+    lista = []
