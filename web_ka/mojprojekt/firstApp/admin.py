04dab469b688d2c05522f1b05c21969f6c88b9bd
Krzysztof Adamek <kadamek@alx.pl>
2022-06-04 12:01:32 +0200

    django - Osoba

--- web_ka/mojprojekt/firstApp/admin.py
+++ web_ka/mojprojekt/firstApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from firstApp.models import Osoba
+
 # Register your models here.
+admin.site.register(Osoba)
--- web_ka/mojprojekt/firstApp/models.py
+++ web_ka/mojprojekt/firstApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Osoba(models.Model):
+    imie = models.CharField(max_length=30)
+    nazwisko = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"({self.id}) {self.imie} {self.nazwisko}"
