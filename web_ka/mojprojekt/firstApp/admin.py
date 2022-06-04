ae38d2808f5864d5642b4e4980fb881ffd59cb70
Krzysztof Adamek <kadamek@alx.pl>
2022-06-04 12:41:03 +0200

    django - Produkt

--- web_ka/mojprojekt/firstApp/admin.py
+++ web_ka/mojprojekt/firstApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from firstApp.models import Osoba
+from firstApp.models import Osoba, Produkt
 
 # Register your models here.
 admin.site.register(Osoba)
+admin.site.register(Produkt)
+
--- web_ka/mojprojekt/firstApp/models.py
+++ web_ka/mojprojekt/firstApp/models.py
@@ -7,3 +7,13 @@ class Osoba(models.Model):
 
     def __str__(self):
         return f"({self.id}) {self.imie} {self.nazwisko}"
+
+
+class Produkt(models.Model):
+    nazwa = models.CharField(max_length=30)
+    opis = models.TextField()
+    cena = models.DecimalField(max_digits=6, decimal_places=2)
+    dostepny = models.BooleanField()
+
+    def __str__(self):
+        return f"({self.id}) {self.nazwa}"
